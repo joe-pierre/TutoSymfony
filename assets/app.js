a9a0d401b0488590e5251fad13536f126f8a3c5a
joe-pierre <gomisjoepierre@gmail.com>
2024-08-20 09:42:48 +0000

    feat: Use webpack instead of assetMapper, install and use select2-bootstrap-theme

--- assets/app.js
+++ assets/app.js
@@ -1,10 +1,27 @@
-import './bootstrap.js';
 /*
  * Welcome to your app's main JavaScript file!
  *
- * This file will be included onto the page via the importmap() Twig function,
- * which should already be in your base.html.twig.
+ * We recommend including the built version of this JavaScript file
+ * (and its CSS file) in your base layout (base.html.twig).
  */
+
+// any CSS you import will output into a single css file (app.css in this case)
 import './styles/app.css';
 
-console.log('This log comes from assets/app.js - welcome to AssetMapper! 🎉');
+import $ from 'jquery';
+
+// Import Select2 with relative path
+import 'select2/dist/js/select2.min.js';
+
+// Import Select2 CSS
+import 'select2/dist/css/select2.min.css';
+
+// Import Bootstrap 5 theme for Select2
+import 'select2-bootstrap-5-theme/dist/select2-bootstrap-5-theme.min.css';
+
+// Set Select2 Bootstrap 5 theme
+$(function () {
+    $('select').select2({
+        theme: 'bootstrap-5'
+    });    
+})
